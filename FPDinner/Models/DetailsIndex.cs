e1375ee34d252997a7efc5fd70ce935c51b02ac5
bszypelow <bszypelow@future-processing.com>
2014-04-16 12:39:15 +0200

    enable nuget packages restore

---
+++ FPDinner/Models/DetailsIndex.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Raven.Abstractions.Indexing;
+using Raven.Client.Indexes;
+
+namespace FPDinner.Models
+{
+    public class DetailsIndex : AbstractTransformerCreationTask<Order>
+    {
+        public DetailsIndex()
+        {
+            TransformResults = orders => from o in orders
+                                         select new Details
+                                         {
+                                             MenuId = o.MenuId,
+                                             Person = o.Person,
+                                             Dinner = LoadDocument<Dinner>("dinners/" + o.Dinner.DinnerId).Name,
+                                             Potatoes = o.Dinner.Potatoes,
+                                             Salad1 = o.SaladIds[0] != 0 ? LoadDocument<Salad>("salads/" + o.SaladIds[0]).Name : string.Empty,
+                                             Salad2 = o.SaladIds[1] != 0 ? LoadDocument<Salad>("salads/" + o.SaladIds[1]).Name : string.Empty,
+                                             Notes = o.Dinner.Notes
+                                         };
+        }
+    }
+}
---
+++ FPDinner/Models/Migrations/OrderV1ToV2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Raven.Client.Listeners;
+using Raven.Json.Linq;
+
+namespace FPDinner.Models.Migrations
+{
+	//public class OrderV1ToV2 : IDocumentConversionListener
+	//{
+	//	public void DocumentToEntity(string key, object entity, RavenJObject document, RavenJObject metadata)
+	//	{
+	//		Order o = entity as Order;
+	//		if (o != null)
+	//		{
+	//			if( metadata.Value<int>("Schema-Version") >= 2)
+	//			{
+	//				return;
+	//			}
+				
+	//			o.Salads = new int[2];
+	//		}
+
+	//		OrderedDinner d = entity as OrderedDinner;
+	//		if (d != null)
+	//		{
+	//			if (metadata.Value<int>("Schema-Version") >= 2)
+	//			{
+	//				return;
+	//			}
+
+	//			d.Dinner = 0;
+	//		}
+	//	}
+
+	//	public void EntityToDocument(string key, object entity, RavenJObject document, RavenJObject metadata)
+	//	{
+	//		Order o = entity as Order;
+	//		OrderedDinner d = entity as OrderedDinner;
+	//		if (o != null || d != null)
+	//		{
+	//			metadata["Schema-Version"] = 2;
+	//		}
+	//	}
+	//}
+}
